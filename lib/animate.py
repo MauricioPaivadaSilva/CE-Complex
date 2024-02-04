7a01749ce1ddd3a77086f20b8075abe073d420ae
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-02-04 14:15:02 -0300

    Início da reescrita das funções de animação do gráfico.

--- lib/animate.py
+++ lib/animate.py
@@ -1,232 +1,102 @@
 #
-#   Iniciando os a chamada das libs necessárias.
+#   Chamada das dependencias necessárias
 #
 
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import numpy as np
-import os
 
 #
-#   Iniciando os a chamada dos plug-ins necessários.
+#   Chamada do plugin necessário
 #
 
-from err import Err as Err
-
-##  Criação de variáveis globais
-
-x = []
-X = []
-y = []
-Y = []
-
-rad = []
-
-xlim_cic = 1.5
-ylim_cic = 1.5
-xlim_cic_ = -xlim_cic
-ylim_cic_ = -ylim_cic
-
-xlim_sin = 62
-ylim_sin = 1.5
-xlim_sin_ = 0
-ylim_sin_ = -ylim_cic
-
+from .err import Err as Err
 
 ##########################################
 ######      Início do código        ######
 ##########################################
 
 class Animate:
-    def __init__(self, frequencia, tipo, test):
-        global x
-        global X
-        global y
-        global Y
-        global rad
+    def __init__(self, frequencia, gf1, gf2, test):
+        self.gf1 = gf1
+        self.gf2 = gf2
+        self.frequencia = frequencia
 
-        global xlim_sin
-        global ylim_sin
-        global xlim_sin_
-        global ylim_sin_
+        self.setup_plots(frequencia)
+        self.animate()
 
-        x.clear()
-        X.clear()
-        y.clear()
-        Y.clear()
-        rad.clear()
-
-        plt.cla()
-        plt.clf()
-
-        if(frequencia != 0.0):
-            num1 = frequencia
-            xlim_sin = num1 + 2
-        else:
-            num1 = 60.0
-            xlim_sin = num1 + 2
-
-        a = 0
-        b = 0
-        i = True
-        Y.append(b)
-        while i:
-            b += 1
-            Y.append(b)
-            if(b == num1):
-                i = False
-        Y.sort()
-        ey = Y
-
-        if(tipo == "sin"):
-            Animate.ConvertInRad(num1, ey, test)
-        else:
-            Animate.animmate_cic(test)
+    ##   Definição de configurações dos gráfcos que são plotados
     
-    def ConvertInRad(num1, eq, test):   #   Conversão dos valores de grau em radianos.
+    def setup_plots(self, frequencia):
 
-        #   Importação das variáveis globais.
+        ### Gráfico da função senoide
 
-        global rad
+        self.sin_line, = self.gf1.plot(
+            [], 
+            [], 
+            color="blue", 
+            lw=2
+        )
+        self.time_line, = self.gf1.plot(
+            [], 
+            [], 
+            color="red", 
+            lw=2
+        )
+        self.gf1.set_xlim(0, (frequencia + 2))
+        self.gf1.set_ylim(-1.2, 1.2)
+        self.gf1.set_xlabel('t (segundos)')
+        self.gf1.set_yticks([-1, 0, 1])
+        self.gf1.set_yticklabels(['-127V', '0V', '127V'])
 
-        for i in eq:
-            r = (i*np.pi)/180
-            rad.append(r)
+        eixo_x = mpatches.FancyArrowPatch(
+            (0, 0),
+            ((frequencia + 2), 0),
+            color='black',
+            mutation_scale=15,
+            arrowstyle='->'
+        )
+        self.gf1.add_patch(eixo_x)
+
+        ### Gráfico da ciclo trigonométrico
         
-        Animate.animmate_sin(num1, eq, test)
+        self.circle, = self.gf2.plot(
+            [],
+            [],
+            color='black'
+        )
+        self.quiver = self.gf2.quiver(
+            0,
+            0,
+            0,
+            0,
+            angles='xy',
+            scale=1,
+            color='black'
+        )
+        self.gf2.scatter(
+            0,
+            0,
+            color='red'
+        )
+        self.gf2.set_xlim(-1.2, 1.2)
+        self.gf2.set_ylim(-1.2, 1.2)
 
-    def animmate_sin(num1, num2, test):
+        eixo_x_cic = mpatches.FancyArrowPatch(
+            (-1.2, 0),
+            (1.2, 0),
+            color='black',
+            mutation_scale=15,
+            arrowstyle='->'
+        )
+        self.gf2.add_patch(eixo_x_cic)
 
-        sin = []
-
-        plt.ion()
-        temp = 0
-
-        try:
-            for i in rad:
-                si = np.sin(i * (num1/4))
-                sin.append(si)
-        except:
-            pass
-
-        while temp <= 60.0:
-
-            plt.cla()
-            plt.clf()
-
-            plt.plot(num2, sin, color="blue")
-            plt.plot([0, temp], [0, 0], color="red", lw=2)
-            plt.xlim(xlim_sin_, xlim_sin)
-            plt.ylim(ylim_sin_, ylim_sin)
-            plt.xlabel('s (segundos)')
-
-            #   Adicionando o eixo x.
-            eixo_x = mpatches.FancyArrowPatch(
-                ((xlim_sin_), 0), 
-                ((xlim_sin), 0), 
-                color='black', 
-                mutation_scale=15, 
-                arrowstyle='->',
-            )
-            plt.gca().add_patch(eixo_x)
-
-            #   Adicionando o eixo y.
-            eixo_y = mpatches.FancyArrowPatch(
-                (0, ylim_sin_),
-                (0, ylim_sin),
-                color="black",
-                mutation_scale=15,
-                arrowstyle='->'
-            )
-            plt.gca().add_patch(eixo_y)
-
-            temp += 0.1
-            plt.pause(0.05)
-        
-        plt.ioff()
-
-        plt.show()
-
-        # print(temp)   #   Condição para leitura de dados
-
-        sin.clear()
-    
-    def animmate_cic(test):
-
-        plt.ion()
-        temp = 0
-        num1 = 10
-        num2 = 0
-
-        while temp <= 60.1:
-            plt.cla()
-            plt.clf()
-
-            raio = 1
-            theta = np.linspace(0, 2*np.pi, 100)
-            x = raio*np.cos(theta)
-            y = raio*np.sin(theta)
-            p = np.sqrt((num1**2)+(num2**2))
-
-            
-            vec_x = num1/p
-            vec_y = num2/p
-
-            if(((num1 > 0) and (num1 < 11)) and (num2 >= 0)):
-                num1 -= 1
-                num2 += 1
-            elif(((num1 <= 0) and (num1 > -11)) and (num2 > 0)):
-                num1 -= 1
-                num2 -= 1
-            elif(((num1 < 0) and (num1 >= -10))):
-                num1 += 1
-                num2 -= 1
-            elif(((num1 >= 0) and (num1 < 11)) and (num2 > -11)):
-                num1 += 1
-                num2 += 1
-
-            plt.plot(x, y, color='black')
-            plt.quiver(
-                0, 
-                0, 
-                vec_x, 
-                vec_y, 
-                angles='xy', 
-                scale_units='xy', 
-                scale=1, 
-                color='black',
-            )
-
-            # Plota o ponto vermelho
-            plt.scatter(0, 0, color='red', label='Ponto Vermelho')
-            
-            plt.xlim(-1.5, 1.5)
-            plt.ylim(-1.5, 1.5)
-
-            #  Adicionando o eixo x.
-            eixo_x = mpatches.FancyArrowPatch(
-                (( -1.5), 0), 
-                (( + 1.5), 0), 
-                color='black', 
-                mutation_scale=15, 
-                arrowstyle='->'
-            )
-            plt.gca().add_patch(eixo_x)
-
-            #   Adicionando o eixo y.
-            eixo_y = mpatches.FancyArrowPatch(
-                (0, -1.5),
-                (0, 1.5),
-                color="black",
-                mutation_scale=15,
-                arrowstyle='->'
-            )
-            plt.gca().add_patch(eixo_y)
-            plt.gca().set_aspect('equal', adjustable='box')
-
-            temp += 0.1
-            plt.pause(0.05)
-
-        plt.ioff()
-
-        plt.show()
+        eixo_y_cic = mpatches.FancyArrowPatch(
+            (0, -1.2),
+            (0, 1.2),
+            color='black',
+            mutation_scale=15,
+            arrowstyle='->'
+        )
+        self.gf2.add_patch(eixo_y_cic)
+        self.gf2.set_aspect('equal', adjustable='box')
