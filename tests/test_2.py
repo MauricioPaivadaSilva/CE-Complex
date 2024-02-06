f56683e70b152b249c0d847c44b8bc55ab15c9f0
Maurício Paiva da Silva <mauriciopaiva.matematica@gmail.com>
2024-02-06 11:45:11 -0300

    Correção de um dos testes para a validação em sistema windows e linux.

--- tests/test_2.py
+++ tests/test_2.py
@@ -5,6 +5,7 @@
 from PIL import Image
 import numpy as np
 import os
+import sys
 
 #
 #   Chamando os arquivos das libs para teste
@@ -27,18 +28,31 @@ comp_2 = "-2-2j"
 
 def test_da_geracao_de_grafico_de_sin_com_x():
     rt.Complex(let_1, True)
-    
-    diretorio_script = os.path.dirname(os.path.abspath(__file__))
-    origin_sin = os.path.join(diretorio_script, '..\\img', 'origin_sin.png')
-    test_img = os.path.join(diretorio_script, '..\\temp', 'test_comparar_grafico_sin.png')
 
-    imagem_grafico1 = Image.open(origin_sin)
-    imagem_grafico2 = Image.open(test_img)
+    if sys.platform.startswith('win'):
+        diretorio_script = os.path.dirname(os.path.abspath(__file__))
+        origin_sin = os.path.join(diretorio_script, '..\\img', 'origin_sin.png')
+        test_img = os.path.join(diretorio_script, '..\\temp', 'test_comparar_grafico_sin.png')
 
-    assert np.array_equal(np.array(imagem_grafico1), np.array(imagem_grafico2))
-    
-    caminho_arquivo = os.path.join(diretorio_script, '..\\temp', 'test_comparar_grafico_sin.png')
-    os.remove(caminho_arquivo)
+        imagem_grafico1 = Image.open(origin_sin)
+        imagem_grafico2 = Image.open(test_img)
+
+        assert np.array_equal(np.array(imagem_grafico1), np.array(imagem_grafico2))
+        
+        caminho_arquivo = os.path.join(diretorio_script, '..\\temp', 'test_comparar_grafico_sin.png')
+        os.remove(caminho_arquivo)
+    else:
+        diretorio_script = os.path.dirname(os.path.abspath(__file__))
+        origin_sin = os.path.join(diretorio_script, '../img', 'origin_sin.png')
+        test_img = os.path.join(diretorio_script, '../temp', 'test_comparar_grafico_sin.png')
+
+        imagem_grafico1 = Image.open(origin_sin)
+        imagem_grafico2 = Image.open(test_img)
+
+        assert np.array_equal(np.array(imagem_grafico1), np.array(imagem_grafico2))
+        
+        caminho_arquivo = os.path.join(diretorio_script, '../temp', 'test_comparar_grafico_sin.png')
+        os.remove(caminho_arquivo)
 
 def test_da_geracao_de_grafico_de_sin_com_y():
     rt.Complex(let_2, True)
